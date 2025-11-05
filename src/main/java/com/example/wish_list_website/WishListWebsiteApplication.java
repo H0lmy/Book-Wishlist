5dfd223189f8bafcea2fcff382003daa7c2cf885
Maksym Shtymak <maksym.shtymak@student.griffith.ie>
2025-11-05 16:59:00 +0200

    Initial commit

---
+++ src/main/java/com/example/wish_list_website/WishListWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.wish_list_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishListWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishListWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wish_list_website/WishListWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wish_list_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishListWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
